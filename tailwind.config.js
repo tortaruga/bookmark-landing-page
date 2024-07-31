8ec6c5fcd2f27549273d452ae2f7c31cb4a69471
tortaruga <134407215+tortaruga@users.noreply.github.com>
2024-07-31 18:50:18 +0200

    updated design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,29 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {}, 
+    extend: {
+      backgroundImage: {
+       'icon-twitter': "url('./images.icon-twitter.svg')",
+       'icon-facebook': "url('./images.icon-facebook.svg')",
+      },
+      flexGrow: {
+        'flex-grow-2': '2'
+      },
+      letterSpacing: {
+        extra: '0.3rem'
+      },
+      fontFamily: {
+         'myFont': ['Rubik, sans-serif'],
+      },
+    colors: {
+      primary: {
+        100: 'hsl(231, 69%, 60%)',
+        200: 'hsl(229, 8%, 60%)',
+        300:  'hsl(229, 31%, 21%)',
+      },
+      accent: 'hsl(0, 94%, 66%)'
+    }
   },
   plugins: [],
 }  
-
+}
