c46f1061d920c7a7e1630c79674d72ab1084d132
tortaruga <134407215+tortaruga@users.noreply.github.com>
2024-08-06 16:09:35 +0200

    finished (i hope)

--- README-template.md
+++ README-template.md
@@ -6,19 +6,15 @@ This is a solution to the [Bookmark landing page challenge on Frontend Mentor](h
 
 - [Overview](#overview)
   - [The challenge](#the-challenge)
-  - [Screenshot](#screenshot)
   - [Links](#links)
-- [My process](#my-process)
   - [Built with](#built-with)
   - [What I learned](#what-i-learned)
   - [Continued development](#continued-development)
-  - [Useful resources](#useful-resources)
 - [Author](#author)
-- [Acknowledgments](#acknowledgments)
-
-**Note: Delete this note and update the table of contents based on what sections you keep.**
 
 ## Overview
+
+do you have any idea how long it took me to complete this challenge? and i'm not even that satisfied with the result! tailwind will be the death of me. i just know it. i feel it in my bones. 
 
 ### The challenge
 
@@ -30,85 +26,29 @@ Users should be able to:
   - The input field is empty
   - The email address is not formatted correctly
 
-### Screenshot
-
-![](./screenshot.jpg)
-
-Add a screenshot of your solution. The easiest way to do this is to use Firefox to view your project, right-click the page and select "Take a Screenshot". You can choose either a full-height screenshot or a cropped one based on how long the page is. If it's very long, it might be best to crop it.
-
-Alternatively, you can use a tool like [FireShot](https://getfireshot.com/) to take the screenshot. FireShot has a free option, so you don't need to purchase it. 
-
-Then crop/optimize/edit your image however you like, add it to your project, and update the file path in the image above.
-
-**Note: Delete this note and the paragraphs above when you add your screenshot. If you prefer not to add a screenshot, feel free to remove this entire section.**
 
 ### Links
 
 - Solution URL: [Add solution URL here](https://your-solution-url.com)
 - Live Site URL: [Add live site URL here](https://your-live-site-url.com)
 
-## My process
-
 ### Built with
 
 - Semantic HTML5 markup
-- CSS custom properties
 - Flexbox
-- CSS Grid
 - Mobile-first workflow
-- [React](https://reactjs.org/) - JS library
-- [Next.js](https://nextjs.org/) - React framework
-- [Styled Components](https://styled-components.com/) - For styles
+- JS
+- Tailwind CSS
 
-**Note: These are just examples. Delete this note and replace the list above with your own choices**
 
 ### What I learned
 
-Use this section to recap over some of your major learnings while working through this project. Writing these out and providing code samples of areas you want to highlight is a great way to reinforce your own knowledge.
-
-To see how you can add code snippets, see below:
-
-```html
-<h1>Some HTML code I'm proud of</h1>
-```
-```css
-.proud-of-this-css {
-  color: papayawhip;
-}
-```
-```js
-const proudOfThisFunc = () => {
-  console.log('🎉')
-}
-```
-
-If you want more help with writing markdown, we'd recommend checking out [The Markdown Guide](https://www.markdownguide.org/) to learn more.
-
-**Note: Delete this note and the content within this section and replace with your own learnings.**
+I think by the end of this challenge tailwind is a bit less of a mystery than it was at the start. I also think ironically I would have completed the challenge a lot faster with regular CSS, but i'm proud of what it looks like after struggling with this damned tailwind so much!   
 
 ### Continued development
 
-Use this section to outline areas that you want to continue focusing on in future projects. These could be concepts you're still not completely comfortable with or techniques you found useful that you want to refine and perfect.
-
-**Note: Delete this note and the content within this section and replace with your own plans for continued development.**
-
-### Useful resources
-
-- [Example resource 1](https://www.example.com) - This helped me for XYZ reason. I really liked this pattern and will use it going forward.
-- [Example resource 2](https://www.example.com) - This is an amazing article which helped me finally understand XYZ. I'd recommend it to anyone still learning this concept.
-
-**Note: Delete this note and replace the list above with resources that helped you during the challenge. These could come in handy for anyone viewing your solution or for yourself when you look back on this project in the future.**
+i want to focus on making the html more readable by reusing components and the js more clean by splitting it up in modules. i tried importing the constants from another js file to make it more tidy, but then it wouldn't work. I'll figure out why in another moment though because i really want to wrap this up after so long.
 
 ## Author
 
-- Website - [Add your name here](https://www.your-site.com)
-- Frontend Mentor - [@yourusername](https://www.frontendmentor.io/profile/yourusername)
-- Twitter - [@yourusername](https://www.twitter.com/yourusername)
-
-**Note: Delete this note and add/remove/edit lines above based on what links you'd like to share.**
-
-## Acknowledgments
-
-This is where you can give a hat tip to anyone who helped you out on this project. Perhaps you worked in a team or got some inspiration from someone else's solution. This is the perfect place to give them some credit.
-
-**Note: Delete this note and edit this section's content as necessary. If you completed this challenge by yourself, feel free to delete this section entirely.**
+- Frontend Mentor - [@tortaruga](https://www.frontendmentor.io/profile/tortaruga)
--- app.js
+++ app.js
@@ -7,149 +7,26 @@ const userEmail = document.querySelector('.user-email');
 const errorMsg = document.querySelector('.error-message');
 const errorWrapper = document.querySelector('.error-wrapper');
 const body = document.querySelector('body');
-
-// console.log('i wanna die'); 
-
-
 const questionBtns = document.querySelectorAll('.question');
 const answers = document.querySelectorAll('.answer');
-// console.log(questionBtns);
-
-questionBtns.forEach(btn => {
-   btn.addEventListener('click', (e) => {
-      let index = Array.from(questionBtns).indexOf(e.currentTarget); 
-      console.log(index);
-
-      Array.from(answers)[index].classList.toggle('hidden'); 
-   })      
-}); 
-
-mobileMenuBtn.addEventListener('click', () => {
-    mobileMenuBtn.classList.toggle('open');
-    if (mobileMenuBtn.classList.contains('open')) {
-      mobileMenuBtn.innerHTML = '<img class="sm:hidden" src="./images/icon-close.svg" alt="icon close menu">'
-      nav.classList.toggle('nav-menu-open');  
-      mobileMenu.classList.toggle('hidden');
-      mobileMenu.classList.toggle('flex'); 
-      logo.src = "./images/logo-white.svg";
-      body.classList.toggle('overflow-hidden');
-    } else {
-        mobileMenuBtn.innerHTML = '<img class="sm:hidden" src="./images/icon-hamburger.svg" alt="icon open menu">';
-      nav.classList.toggle('nav-menu-open');  
-      mobileMenu.classList.toggle('hidden');
-      mobileMenu.classList.toggle('flex'); 
-      logo.src = "./images/logo-bookmark.svg"; 
-      body.classList.toggle('overflow-hidden');
-
-    } 
-    
-})
-
-let emailValue;
-
 const errorIcon = document.querySelector('.error-icon');
-
-contactForm.addEventListener('submit', (e) => {
-   e.preventDefault();
-  emailValue = userEmail.value;
-  const emailRegex = /^[A-Za-z0-9-%_.]+@[A-Za-z0-9.-]+\.[a-z]{2,}$/;
-  if (!emailRegex.test(emailValue)) {
-   errorMsg.classList.remove('hidden');   
-   errorIcon.classList.remove('hidden');  
-   errorWrapper.classList.toggle('bg-accent');
-   errorWrapper.classList.toggle('p-0.5');
-  } else {
-   errorMsg.classList.add('hidden'); 
-   errorIcon.classList.add('hidden');
-   errorWrapper.classList.toggle('p-0.5');
-   errorWrapper.classList.toggle('bg-accent');
-  }
-})
-
 const tab1 = document.querySelector('.tab-one');
 const tab2 = document.querySelector('.tab-two');
 const tab3 = document.querySelector('.tab-three');
 const tabLink1 = document.querySelector('.tab-link-one');
 const tabLink2 = document.querySelector('.tab-link-two');
 const tabLink3 = document.querySelector('.tab-link-three');
-
-tabLink1.addEventListener('click', (e) => {
-  e.preventDefault();
-  tabLink1.classList.add('underlined');
-  tabLink2.classList.remove('underlined');
-  tabLink3.classList.remove('underlined');
-
-  tab1.classList.remove('hidden');
-  tab1.classList.add('sm:flex');
-  tab2.classList.add('hidden');
-  tab2.classList.remove('sm:flex');
-  tab3.classList.add('hidden');
-  tab3.classList.remove('sm:flex');
-})
-
-tabLink2.addEventListener('click', (e) => {
-  e.preventDefault();
-
-  tabLink2.classList.add('underlined');
-  tabLink1.classList.remove('underlined');
-  tabLink3.classList.remove('underlined');
-
-  tab2.classList.remove('hidden');
-  tab2.classList.add('sm:flex');
-  tab1.classList.add('hidden');
-  tab1.classList.remove('sm:flex');
-  tab3.classList.add('hidden');
-  tab3.classList.remove('sm:flex');
-
-  
-})
-
-tabLink3.addEventListener('click', (e) => { 
-  e.preventDefault();
-
-  tabLink3.classList.add('underlined');
-  tabLink1.classList.remove('underlined');
-  tabLink2.classList.remove('underlined');
-
-  tab3.classList.remove('hidden');
-  tab3.classList.add('sm:flex');
-
-  tab2.classList.add('hidden');
-  tab2.classList.remove('sm:flex');
-
-  tab1.classList.add('hidden');
-  tab1.classList.remove('sm:flex');
-
-})
-
-
-
 const iconFb = document.querySelectorAll('.icon-fb');
 const iconX = document.querySelectorAll('.icon-x');
- 
-iconFb.forEach(icon => {
-  icon.addEventListener('mouseenter', () => {
-    icon.src = "./images/fb-hover.svg";
-  })
-
-  icon.addEventListener('mouseleave', () => {
-    icon.src = "./images/icon-facebook.svg"; 
-  })
-
-})
-
-iconX.forEach(icon => {
-  icon.addEventListener('mouseenter', () => {
-    icon.src = "./images/x-hover.svg";
-  })
-  
-  icon.addEventListener('mouseleave', () => {
-    icon.src = "./images/icon-twitter.svg"; 
-  }) 
-  
-})
-
 const arrows = document.querySelectorAll('.arrow');
+
+// faq section   
+questionBtns.forEach(btn => {
+   btn.addEventListener('click', (e) => {
+      let index = Array.from(questionBtns).indexOf(e.currentTarget); 
+      Array.from(answers)[index].classList.toggle('hidden'); 
+   })      
+}); 
 
 arrows.forEach(arrow => {
   arrow.addEventListener('mouseenter', () => {
@@ -159,4 +36,156 @@ arrows.forEach(arrow => {
   arrow.addEventListener('mouseleave', () => {
     arrow.src = "./images/icon-arrow.svg";
   }) 
+})
+
+// mobile nav menu
+
+const closeMenuImg = '<img class="sm:hidden" src="./images/icon-close.svg" alt="icon close menu">';
+const openMenuImg = '<img class="sm:hidden" src="./images/icon-hamburger.svg" alt="icon open menu">';
+
+function updateMenuIcon(icon) {
+  mobileMenuBtn.innerHTML = icon;
+}
+
+function updateSrc(img, path) {
+   img.src = path;
+}
+
+function handleNavMenu() {
+  nav.classList.toggle('nav-menu-open');  
+  mobileMenu.classList.toggle('hidden');
+  mobileMenu.classList.toggle('flex'); 
+  body.classList.toggle('overflow-hidden');
+}
+
+mobileMenuBtn.addEventListener('click', () => {
+  mobileMenuBtn.classList.toggle('open');
+
+    if (mobileMenuBtn.classList.contains('open')) {
+      updateMenuIcon(closeMenuImg);
+      handleNavMenu();
+      updateSrc(logo, "./images/logo-white.svg");
+    } else {
+      updateMenuIcon(openMenuImg);
+      handleNavMenu();
+      updateSrc(logo, "./images/logo-bookmark.svg");
+    }   
+})
+
+// email validation
+
+let emailValue;
+const emailRegex = /^[A-Za-z0-9-%_.]+@[A-Za-z0-9.-]+\.[a-z]{2,}$/;
+ 
+function handleErrorStyle() {
+  errorWrapper.classList.toggle('bg-accent');
+  errorWrapper.classList.toggle('p-0.5');
+}
+
+function showElement(element, flex) {
+  element.classList.remove('hidden');
+
+  if (flex === 'flex') {
+    element.classList.add('sm:flex');
+  } else if (flex === 'noFlex') {
+    return;
+  }
+}
+
+function hideElement(element, flex) {
+  element.classList.add('hidden');
+
+  if (flex === 'flex') {
+    element.classList.remove('sm:flex');
+  } else if (flex === 'noFlex') {
+    return;
+  }
+}
+
+contactForm.addEventListener('submit', (e) => {
+   e.preventDefault();
+
+  emailValue = userEmail.value;
+ 
+  if (!emailRegex.test(emailValue)) {
+    showElement(errorMsg, 'noFlex');
+    showElement(errorIcon, 'noFlex');  
+   handleErrorStyle();
+  } else {
+
+    hideElement(errorMsg, 'noFlex');
+    hideElement(errorIcon, 'noFlex');
+   handleErrorStyle();
+  }
+})
+
+// feature tabs
+
+function underline(link) {
+  link.classList.add('underlined');
+}
+
+function removeUnderline(link) {
+  link.classList.remove('underlined');
+}
+
+tabLink1.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  underline(tabLink1);
+  removeUnderline(tabLink2);
+  removeUnderline(tabLink3);
+
+  showElement(tab1, 'flex');
+  hideElement(tab2, 'flex');
+  hideElement(tab3, 'flex');
+})
+
+tabLink2.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  underline(tabLink2);
+  removeUnderline(tabLink1);
+  removeUnderline(tabLink3);
+
+  showElement(tab2, 'flex');
+  hideElement(tab3, 'flex');
+  hideElement(tab1, 'flex');
+})
+
+tabLink3.addEventListener('click', (e) => { 
+  e.preventDefault();
+
+  underline(tabLink3);
+  removeUnderline(tabLink2);
+  removeUnderline(tabLink1);
+
+  showElement(tab3, 'flex');
+  hideElement(tab2, 'flex');
+  hideElement(tab1, 'flex');
+
+})
+
+// social icons hover effect
+ 
+iconFb.forEach(icon => {
+  icon.addEventListener('mouseenter', () => {
+    updateSrc(icon, "./images/fb-hover.svg");
+  })
+
+  icon.addEventListener('mouseleave', () => {
+    updateSrc(icon, "./images/icon-facebook.svg");
+  })
+
+})
+
+iconX.forEach(icon => {
+  icon.addEventListener('mouseenter', () => {
+    updateSrc(icon, "./images/x-hover.svg");
+  })
+  
+  icon.addEventListener('mouseleave', () => {
+    updateSrc(icon, "./images/icon-twitter.svg");
+  }) 
+  
 })
